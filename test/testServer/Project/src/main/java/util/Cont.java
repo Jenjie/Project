1481abf0c90f905041c9a5035b1e8762ab025226
Jenjie <s5704062630014@email.kmutnb.ac.th>
2018-07-22 15:33:17 +0700

    commit message

--- test/testServer/Project/src/main/java/util/Cont.java
+++ test/testServer/Project/src/main/java/util/Cont.java
@@ -1,6 +1,6 @@
 package util;
 
 public class Cont {
-    public static final String urlConnect = "jdbc:mysql://localhost:3306/test" +
-            "?user=root&characterEncoding=UTF-8";
+    public static final String urlConnect = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net:3306/heroku_bc560e3cff14c41" +
+            "?user=bff338ea9d4e3f&password=42d3c006";
 }
